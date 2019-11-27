ddd73e44c6bb85a6103058b04869b9b0db4ead57
GHOST <GHOST@GHOST-PC>
2019-11-26 22:19:43 -0200

    #Commit - Atualização simple gui cliente

---
+++ Clinicax/src/gui/GRemover.java
@@ -0,0 +1,26 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import repositorio.ControlRepMedicamento;
+
+public class GRemover extends JFrame implements ActionListener {
+	
+	ControlRepMedicamento controlRepMed;
+	
+	//recebo ma busca qualquer busco em qual repositorio apaagar
+	//for()
+	
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Clinicax/src/guiCadastrar/GCadastroCliente.java
+++ Clinicax/src/guiCadastrar/GCadastroCliente.java
@@ -16,6 +16,7 @@ import entidade.Animal;
 import entidade.Cliente;
 import entidade.Endereco;
 import entidade.Pessoa;
+import repositorio.ControlRepCliente;
 import repositorio.RepositorioCliente;
 
 public class GCadastroCliente extends JFrame implements ActionListener{
@@ -33,11 +34,15 @@ public class GCadastroCliente extends JFrame implements ActionListener{
 	GCadastro gCadastro;
 	
 	
-	RepositorioCliente repclientes = new RepositorioCliente();
+	ControlRepCliente repclientes = new ControlRepCliente();
 	
 	public GCadastroCliente()
 	{
+		
+		
 		super("Cadastro Cliente");
+		
+		//Pessoa cliente = new Cliente(nome.getText(), endereco, title, title, null);
 		
 		Container container = getContentPane();
 		
@@ -46,51 +51,78 @@ public class GCadastroCliente extends JFrame implements ActionListener{
 		container.add(new JLabel ("Nome"));
 		nome =  new JTextField(30);
 		container.add(nome);
+		String teste = (nome.getText());
+		cliente.setNome(teste);
+		
+		
 		
 		container.add(new JLabel ("CPF:"));
 		cpf = new JTextField(16);
 		container.add(cpf);
+		String aux = cpf.getText();
+		cliente.setCpf(aux);
 		
 		container.add(new JLabel("Rua"));
 		rua = new JTextField(20);
 		container.add(rua);
+		String r = rua.getText();
+		endereco.setRua(r);
 		
 		container.add(new JLabel("Bairro"));
 		bairro = new JTextField(20);
 		container.add(bairro);
+		String b = bairro.getText();
+		endereco.setBairro(b);
 		
 		container.add(new JLabel("Cep"));
 		cep = new JTextField(16);
 		container.add(cep);
+		String c = cep.getText();
+		endereco.setCep(c);
 		
 		container.add(new JLabel("Estado"));
 		estado = new JTextField(20);
 		container.add(estado);
+		String e = estado.getText();
+		endereco.setEstado(e);
 		
 		container.add(new JLabel("Numero"));
 		numero = new JTextField(20);
 		container.add(numero);
+		String n = numero.getText();
+		endereco.setNumero(n);
 		
 		container.add(new JLabel("País"));
 		pais = new JTextField(20);
 		container.add(pais);
+		String p = pais.getText();
+		endereco.setPais(p);
 		
 		container.add(new JLabel("Nome"));
 		nomea = new JTextField(30);
-		container.add(nome);
+		container.add(nomea);
+		String an = nomea.getText();
+		animal.setNome(an);
 		
 		container.add( new JLabel("Raça"));
 		raca = new JTextField();
 		container.add(raca);
+		String ra = raca.getText();
+		animal.setRaca(ra);
 		
 		
 		container.add(new JLabel("Idade"));
 		idade = new JTextField();
 		container.add(idade);
+		String id = idade.getText();
+		animal.setIdade(id);
 		
 		container.add(new JLabel("Especie"));
 		especie  = new JTextField();
 		container.add(especie);
+		String esp = especie.getText();
+		animal.setEspecie(esp);
+		
 		
 		adicionar = new JButton("OK");
 		container.add(adicionar);
@@ -120,10 +152,14 @@ public class GCadastroCliente extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		
 		if(e.getSource() == adicionar)
-		{			
-			endereco = new Endereco(rua.getText(),bairro.getText(),cep.getText(),estado.getText(),numero.getText(),pais.getText());
-			animal= new Animal(nomea.getText(),raca.getText(),idade.getText(),especie.getText());
+		{		
+			//devo verificar construtor
+			
+			endereco = new Endereco(rua.getText(),bairro.getText(),cep.getText(),estado.getText(),numero.getText(),pais.getText(), title);
+			animal= new Animal(nome.getText(),raca.getText(),idade.getText(),especie.getText(), cliente, null);
 			cliente = new Cliente(nome.getText(),endereco,animal,cpf.getText());
+			ControlRepCliente controlRepCliente;
+			controlRepCliente.adicionar(cliente);
 			gCadastro = new GCadastro();
 			dispose();			
 		}
